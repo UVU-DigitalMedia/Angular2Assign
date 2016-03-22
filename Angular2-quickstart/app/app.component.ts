fce8e1b1cf76d4a7d66c09df3eacb9f62a6008d5
Dustin Robertson <dustinuriah@gmail.com>
2016-03-22 15:41:57 -0600

    Introduction

---
+++ Angular2-quickstart/app/app.component.ts
@@ -0,0 +1,26 @@
+import {Component} from 'angular2/core';
+
+interface Hero {
+  id: number;
+  name: string;
+}
+
+public hero: Hero = {
+  id: 1,
+  name: 'Windstorm'
+};
+
+
+@Component({
+    selector: 'my-app',
+    template:`
+      <h1>{{title}}</h1>
+      <h2>{{hero.name}} details!</h2>
+      <div><label>id: </label>{{hero.id}}</div>
+      <div><label>name: </label>{{hero.name}}</div>
+      `
+})
+export class AppComponent {
+  public title = 'Tour of Heroes';
+  public hero = 'Windstorm';
+}
